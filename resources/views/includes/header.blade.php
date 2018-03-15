426ba09a04ecef7a1164885a2cfbfe868a34a707
Christo Mitov <christo.mitov@gmail.com>
2018-03-15 00:07:28 -0400

    update download links and ga events

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
   <div class="container-lrg">
     <div class="flex col-12 spread">
       <a href="/" class="logo primary-color launchaco-builder-hoverable">
-        <img style="float: left" src="/images/icon128.png"/>BrowserTunes
+        <img style="float: left" src="/images/icon128.png"/>BrowserTunes<small style="position: absolute; padding-left:5px; font-size:18px; bottom:-10px;">beta</small>
       </a>
       <a href="mailto:hello@browsertunes.com" class="nav-link secondary-color mr0 email header-email accent-bg">Contact</a>
     </div>
