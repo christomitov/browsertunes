630b742ab78e3d4bd3c2a90a8a88bcc86fab9017
Christo Mitov <christo.mitov@gmail.com>
2018-03-19 00:57:40 -0400

    update api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +16,68 @@ use GuzzleHttp\Client;
 */
 
 
-
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('search', function (Request $request) {
+    $_YOUTUBE_API_KEY = 'AIzaSyC-zx7mRdw67od9k5ZsXu0VAjhw1Xjd4dM';
+    $_YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
+    $_SPOTIFY_API_URL = 'https://api.spotify.com/v1/search';
+
+    $client = new Client();
+
+    $isYoutube = isset($request->all()['youtube']) && $request->all()['youtube'] == 1;
+    $isSpotify = isset($request->all()['spotify']) && $request->all()['spotify'] == 1;
+
+    $youtube = $client->getAsync($_YOUTUBE_API_URL, [
+        'query' => [
+            'part' => 'snippet',
+            'q' => $request->all()['terms'],
+            'key' => $_YOUTUBE_API_KEY,
+            'type' => 'video',
+            'maxResults' => 5
+        ]
+    ]);
+
+    $spotify = $client->getAsync($_SPOTIFY_API_URL, [
+        'query' => [
+            'q' => $request->all()['terms'],
+            'type' => 'track',
+            'market'=> 'from_token',
+            'limit' => 5
+        ],
+        'headers' => [
+            'Authorization' => 'Bearer ' . (isset($request->all()['spotify_access_token']) ? $request->all()['spotify_access_token'] : '')
+        ]
+    ]);
+
+
+    $promises = [];
+
+    if ($isYoutube) {
+        $promises['youtube'] = $youtube;
+    }
+
+    if ($isSpotify) {
+        $promises['spotify'] = $spotify;
+    }
+
+    // Wait for the requests to complete, even if some of them fail
+    $results = Promise\settle($promises)->wait();
+
+    $response = [];
+
+    if (isset($results['youtube']['value'])) {
+        $response['youtube'] = json_decode($results['youtube']['value']->getBody())->items;
+    }
+
+    if (isset($results['spotify']['value'])) {
+        $response['spotify'] =json_decode($results['spotify']['value']->getBody(), true)['tracks']['items'];
+    }
+
+    return $response;
+});
 
 // Gets an access token and refresh token based on authorization code
 Route::get('token/{code}', function ($code) {
