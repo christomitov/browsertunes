f05e77a1e779e544c5a81837639cb8546d2ddee4
Christo Mitov <christo.mitov@gmail.com>
2018-03-27 22:20:03 -0400

    overhaul of layout

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,10 +3,9 @@
   <div class="container-lrg">
     <div class="flex col-12 spread">
       <a href="/" class="logo primary-color launchaco-builder-hoverable">
-        <img style="float: left" src="/images/icon128.png"/>BrowserTunes<small style="position: absolute; padding-left:5px; font-size:18px; bottom:-10px;">beta</small>
+        <img style="float: left" src="/images/icon128.png"/>BrowserTunes
       </a>
       <div>
-        <a href="/" class="nav-link secondary-color mr0 ">Home</a>
         <a href="/info" class="nav-link secondary-color mr0 ">Info</a>
         {{--  <a href="#pricing" class="nav-link secondary-color mr0 email header-email accent-bg">Pricing</a>  --}}
         {{--  <a href="#faq" class="nav-link secondary-color mr0 email header-email accent-bg">FAQ</a>  --}}
