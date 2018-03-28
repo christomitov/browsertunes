7d93ae0d4d1dc45d899f06914246a362c7ac76df
Christo Mitov <christo.mitov@gmail.com>
2018-03-27 22:24:33 -0400

    overhaul of layout

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -2,12 +2,14 @@
 @section('content')
 @include('includes.header')
 
-<section id="features" class="section coral-white container-lrg">
-  <h1>What is this?</h1>
-  <p class="subheading secondary-color mt20 launchaco-builder-hoverable">
-    BrowserTunes is developed and maintained by just me, Christo Mitov. I enjoy listening to music while I code but having to context switch
-    became an issue. So I created BrowserTunes as a way to listen to whatever I wanted on the internet without leaving the tab that I was in.
-  </p>
+<section id="info" class="section coral-white">
+  <div class="col-12 container mb40">
+    <h1>What is this?</h1>
+    <p class="subheading secondary-color mt20 launchaco-builder-hoverable">
+      BrowserTunes is developed and maintained by just me, Christo Mitov. I enjoy listening to music while I code but having to context switch
+      became an issue. So I created BrowserTunes as a way to listen to whatever I wanted on the internet without leaving the tab that I was in.
+    </p>
+  </div>
 </section>
 
 
